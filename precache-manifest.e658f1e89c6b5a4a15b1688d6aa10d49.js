86d5b53c8a6569ba229e14bf742aba554d04a69e
Ivan <ivan.kavtarev@mail.ru>
2020-08-18 15:01:03 +0300

    Updates

---
+++ precache-manifest.e658f1e89c6b5a4a15b1688d6aa10d49.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7f18eb432373e5a8e711b455ffcfa6",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "9e70bca6c4117c79f699",
+    "url": "/store/static/css/0.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "652ed475f582ca1330cf",
+    "url": "/store/static/css/6.d15c2b9b.chunk.css"
+  },
+  {
+    "revision": "20d45c7028509b3ab767",
+    "url": "/store/static/css/7.8348a87e.chunk.css"
+  },
+  {
+    "revision": "ed9d09d823e610b6236d",
+    "url": "/store/static/css/main.09ea72c9.chunk.css"
+  },
+  {
+    "revision": "9e70bca6c4117c79f699",
+    "url": "/store/static/js/0.41ecb8b1.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/store/static/js/0.41ecb8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeee4a9de74997525186",
+    "url": "/store/static/js/3.94251b85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/store/static/js/3.94251b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2cf03d53f5a203d3e9",
+    "url": "/store/static/js/4.9d92e994.chunk.js"
+  },
+  {
+    "revision": "dd1844959e344aeb167b",
+    "url": "/store/static/js/5.a357ae6f.chunk.js"
+  },
+  {
+    "revision": "652ed475f582ca1330cf",
+    "url": "/store/static/js/6.f658ab3c.chunk.js"
+  },
+  {
+    "revision": "20d45c7028509b3ab767",
+    "url": "/store/static/js/7.468e9df5.chunk.js"
+  },
+  {
+    "revision": "ed9d09d823e610b6236d",
+    "url": "/store/static/js/main.9b449e3d.chunk.js"
+  },
+  {
+    "revision": "703f1a4165237f77eef5",
+    "url": "/store/static/js/runtime-main.eb69c520.js"
+  },
+  {
+    "revision": "c55b828a785158eca968c773763bed4b",
+    "url": "/store/static/media/cardimg.c55b828a.jpg"
+  },
+  {
+    "revision": "4cc848b538580780aa52b50a7a18e1fa",
+    "url": "/store/static/media/preloader.4cc848b5.gif"
+  }
+]);
